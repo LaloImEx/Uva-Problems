a7385878831f5d2184537b74f00f44f3713adb28
Angel Eduardo <53704332+LaloImEx@users.noreply.github.com>
2022-11-21 20:21:57 -0600

    Create 11991 - Easy Problem from Rujia Liu.cpp

---
+++ Introduction Problems/11991 - Easy Problem from Rujia Liu.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <vector>  
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int size,cases,inp, buscador;
+  vector<vector<int> > vec;
+  
+  while(cin>>size>>cases)
+  {    
+    vec.assign(1000000, vector<int>());
+	  for(int x=1;x<=size; x++)
+	  {
+	    cin>>inp;
+		  vec[inp].push_back(x);
+	  }
+    
+	  for(int t = 0; t < cases; t++)
+	  {
+		  cin>>inp>>buscador;
+
+		  if(inp-1 < vec[buscador].size() )
+		  {
+			  cout<<vec[buscador][inp-1]<<"\n";
+		  }
+		else{
+			cout<<"0\n";
+		  }
+	  }      
+  }
+}
