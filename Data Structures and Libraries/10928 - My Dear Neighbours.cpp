e7b8950c6633c7126de3721cf92f09ae4e477b1a
Angel Eduardo <53704332+LaloImEx@users.noreply.github.com>
2023-03-25 13:27:23 -0600

    Update 10928 - My Dear Neighbours.cpp

--- Data Structures and Libraries/10928 - My Dear Neighbours.cpp
+++ Data Structures and Libraries/10928 - My Dear Neighbours.cpp
@@ -1,67 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 string linea; 
 
+int espacios(string s)
+{
+    int count = 1;
+    for(int as = 0; as < s.length(); as++)
+    {
+        if(s[as] == ' ')count++;        
+    }
+    return count;
+}
+
 int main()
 {
-    int t, np, c = 0, x = 0, last= 0;
+    int t, np, c = 0, x = 0, last= 0,asz;
     cin>>t;
 
     for(int ss = 0; ss<t; ss++)
     {
         cin>>np;
         int ar[np+1];
-       
         while(x < np+1)
         {
             getline(cin, linea);        
             last = espacios(linea);
-    
-            if(last > 0){ ar[x] = last }
+            if(last > 0){ ar[x] = last;}
             x++;
         }
         
-        x = 1;
-        c = 0;
-
-        int arrSize = *(&ar + 1) - ar;
+        x = 1,c = 0;
+        asz = *(&ar + 1) - ar;
         linea = "";
 
-        while(x < arrSize)
+        while(x < asz)
         {
-            for(int o = 1; o < arrSize; o++)
+            for(int o = 1; o < asz; o++)
             {
-                if(ar[o] == x)
-                {
-                    linea += to_string(o)+" ";
-                }
+                if(ar[o] == x)linea += to_string(o)+" ";
             }
 
-            if(linea != "")
-            {
-                x = sizeof(ar);
-            }
-
+            if(linea != "")x = sizeof(ar);        
             x++;
         }
         x = 0;
-
         linea.erase(linea.length()-1);
         cout<<linea<<"\n";   
     }
-}
-
-int espacios(string s)
-{
-    int count = 1;
-
-    for(int as = 0; as < s.length(); as++)
-    {
-        if(s[as] == ' ')
-        {
-            count++;
-        }
-    }
-
-    return count;
 }
